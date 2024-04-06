e1dbb1cae0da852ec7c2883b2a8adb0ef39bb2f3
Morgan-B3 <boutroism@gmail.com>
2024-04-06 19:18:03 +0200

    ✨ added scroll to top functionality on navigate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,26 +4,27 @@ import ShowProject from './pages/ShowProject'
 import ShowUser from './pages/ShowUser'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Form from './components/Form.js';
+import ScrollToTop from './components/ScrollToTop.js';
 // import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <BrowserRouter>
-    {/* <AuthProvider> */}
-    <Routes>
-      <Route path='/' element={<Home />} />
+      <ScrollToTop>
+        <Routes>
+          <Route path='/' element={<Home />} />
 
-      <Route path='/create' element={<Form content='newProject' />} />
+          <Route path='/create' element={<Form content='newProject' />} />
 
-      <Route path='/login' element={<Form content='login' />} />
-      <Route path='/register' element={<Form content='register' />} />
+          <Route path='/login' element={<Form content='login' />} />
+          <Route path='/register' element={<Form content='register' />} />
 
-      <Route path='/project/:id' element={<ShowProject />} />
-      <Route path='/user/:id' element={<ShowUser />} />
+          <Route path='/project/:id' element={<ShowProject />} />
+          <Route path='/user/:id' element={<ShowUser />} />
 
-      <Route path='*' element={<Form content="notFound" />} />
-    </Routes>
-    {/* </AuthProvider> */}
+          <Route path='*' element={<Form content="notFound" />} />
+        </Routes>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
