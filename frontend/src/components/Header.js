c6e6629ffea87ed3a86f91c27d0d8cb7ff24f525
Morgan-B3 <boutroism@gmail.com>
2024-04-09 11:53:57 +0200

    💄 FRONT - CSS cleanup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -51,7 +51,7 @@ function Header() {
         </li>
 
         <li className="item slideInDown-4" >
-          <p className="item-p" onClick={()=>navigate('/')}>Recherche</p>
+          <p className="item-p" onClick={()=> (handleShowTiret(),navigate('/'))}>Recherche</p>
         </li>
         {/* {user ? 
         <li className="item slideInDown-5" >
